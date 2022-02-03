3e11e90d1ff777bb9927494468a2cfa7b0ca7863
Honza Javorek <mail@honzajavorek.cz>
2022-02-03 12:44:09 +0100

    move things around and make tests pass, lol

--- juniorguru/models/__init__.py
+++ juniorguru/models/__init__.py
@@ -1,5 +1,5 @@
 from juniorguru.models.base import db, retry_when_db_locked, with_db, json_dumps
-from juniorguru.models.job import Job, JobDropped, JobError, JobMetric, EMPLOYMENT_TYPES
+from juniorguru.models.job import Job, EMPLOYMENT_TYPES
 from juniorguru.models.metric import Metric
 from juniorguru.models.story import Story
 from juniorguru.models.supporter import Supporter
@@ -9,13 +9,11 @@ from juniorguru.models.topic import Topic
 from juniorguru.models.club import ClubMessage, ClubUser, ClubPinReaction
 from juniorguru.models.event import Event, EventSpeaking
 from juniorguru.models.company import Company
-from juniorguru.models.employment import Employment
 from juniorguru.models.transaction import Transaction
 from juniorguru.models.podcast import PodcastEpisode
 
 
-__all__ = [db, Job, JobDropped, JobError, JobMetric, Metric, Story, Supporter,
-           LastModified,
+__all__ = [db, Job, Metric, Story, Supporter, LastModified,
            retry_when_db_locked, SpiderMetric, EMPLOYMENT_TYPES,
            Topic, ClubMessage, ClubUser, ClubPinReaction, Event, EventSpeaking,
-           Company, with_db, Employment, json_dumps, Transaction, PodcastEpisode]
+           Company, with_db, json_dumps, Transaction, PodcastEpisode]
